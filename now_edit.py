eca11ab1bf6e82f69ce3ffb247ff68ab4f6f45a9
hykk-git <gratia1206@naver.com>
2025-03-11 02:46:09 +0000

    feat: 클래스 추상화 분리

--- now_edit.py
+++ now_edit.py
@@ -2,71 +2,41 @@ import math
 from abc import * 
 import random
 
-class ShootingGame:
-    def __init__(self):
-        self.game_area = GameArea()
-        self.bullets = []
-        self.gun = Gun(self.bullets)
-        self.player_status = PlayerStatus(score=0, life=3)
-        self.enemies = []
-        self.running = True
-    
-    def spawn_enemy(self):
-        if self.running:
-            self.enemies.append(Enemy())
-    
-    def update(self):
-        if not self.running:
-            return
-        
-        self.gun.update_position()
-        for bullet in self.bullets[:]:
-            bullet.update_position()
-            if bullet.point_y < 0:
-                self.bullets.remove(bullet)
-        
-        for enemy in self.enemies[:]:
-            enemy.update_position()
-            if enemy.point_y >= self.game_area.height:
-                self.enemies.remove(enemy)
-                self.player_status.lose_life()
-                if self.player_status.life <= 0:
-                    self.running = False
-        
-        for bullet in self.bullets[:]:
-            for enemy in self.enemies[:]:
-                if bullet.is_collision(enemy):
-                    self.bullets.remove(bullet)
-                    self.enemies.remove(enemy)
-                    self.player_status.update_score()
-    
-    def run(self):
-        while self.running:
-            self.spawn_enemy()
-            self.update()
-            print(f"Score: {self.player_status.score}, Life: {self.player_status.life}")
-    
-    def start(self):
-        self.run()
-
-class GameObject(ABC):
+class GameObject:
     def __init__(self, name):
         self.name = name
 
+    def create_object(self):
+        pass
+
+class ShootingGame:
+    # 내부에 GameObject 객체를 갖고 있음(합성)
+    obj = GameObject()
+    def start(self):
+        # 게임 시작시 필요한 행동
+        # 1. obj 생성해라
+        self.obj.create_object()
+    
+    def player_input(self):
+        # 플레이어가 fire 누르면 총알 나가게
+        GameObject.fire()
+        pass
+
 class Visible(GameObject, ABC):
-    def __init__(self, name, point_x, point_y, size):
-        super().__init__(name)
+    def __init__(self, point_x, point_y, size):
         self.point_x = point_x
         self.point_y = point_y
         self.size = size
     
     @abstractmethod
     def get_position(self):
+        # 자기 위치 반환 함수
+        # 충돌 확인할 때 쓰임- 인자 순서가 x1, y1, x2, y2
         pass
 
 class Movable(Visible, ABC):
-    def __init__(self, name, point_x, point_y, size, speed):
-        super().__init__(name, point_x, point_y, size)
+    def __init__(self, point_x, point_y, size, speed):
+        super().__init__(point_x, point_y, size)
         self.speed = speed
     
     @abstractmethod
@@ -79,17 +49,16 @@ class Collidable(Movable):
         a1, b1, a2, b2 = unit.get_position()
         return x2 >= a1 and y2 >= b1 and a2 >= x1 and b2 >= y1
 
-class Gun(Collidable):
+class Gun(Visible):
     def __init__(self, bullets):
         super().__init__("Gun", 300, 800, 20, 0)
         self.max_bullet = 3
         self.bullets = bullets
-    
-    def update_position(self):
-        self.point_x, self.point_y = GameArea.frame_size[0] // 2, GameArea.frame_size[1]
-    
+        self.point_x = GameArea.frame_size[0]//2
+        self.point_y = GameArea.frame_size[1]
+
     def get_position(self):
-        return self.point_x, self.point_y
+        return self.point_x, self.point_y, self.point_x + self.size, self.point_y + self.size
     
     def fire(self, angle):
         if len(self.bullets) >= self.max_bullet:
@@ -102,12 +71,12 @@ class Bullet(Collidable):
     def __init__(self, angle):
         super().__init__("Bullet", 300, 800, 5, self.speed)
         self.angle = angle
-        self.dx = self.speed * math.sin(math.radians(self.angle))
-        self.dy = -self.speed * math.cos(math.radians(self.angle))
+        self.point_x = GameArea.frame_size[0]//2
+        self.point_y = GameArea.frame_size[1]
     
     def update_position(self):
-        self.point_x += self.dx
-        self.point_y += self.dy
+        self.point_x += self.speed * math.sin(math.radians(self.angle))
+        self.point_y -= self.speed * math.cos(math.radians(self.angle))
     
     def reflex(self):
         self.angle = -self.angle
@@ -137,7 +106,7 @@ class GameArea(Visible):
         self.width, self.height = self.frame_size
     
     def get_position(self):
-        return self.point_x, self.point_y, self.width, self.height
+        return 0, 0, self.width, self.height
 
 class PlayerStatus(GameObject):
     def __init__(self, score, life):
