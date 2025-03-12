b264ce4da8046a904de5332750839848455b71e5
hykk-git <gratia1206@naver.com>
2025-03-12 01:54:05 +0000

    feat: 충돌 관리 클래스 추가 중

--- now_edit.py
+++ now_edit.py
@@ -3,6 +3,7 @@ from abc import *
 import random
 
 class GameObject:
+
     def __init__(self, name):
         self.name = name
 
@@ -52,7 +53,7 @@ class Movable(Visible, ABC):
 
 class Collidable(Movable):
     # 충돌시 이벤트가 발생하는 객체-> 누구랑 충돌했는지 확인해야 함
-    def is_collision(self, unit):
+    def is_collide_at(self, unit):
         x1, y1, x2, y2 = self.get_position()
         a1, b1, a2, b2 = unit.get_position()
         return x2 >= a1 and y2 >= b1 and a2 >= x1 and b2 >= y1
@@ -117,6 +118,7 @@ class GameFrame(Visible):
         return 0, 0, self.width, self.height
 
 class PlayerStatus(GameObject):
+    # User Info 관리 - DB에 저장 필요
     def __init__(self, score, life):
         self.score = score
         self.life = life
@@ -127,40 +129,55 @@ class PlayerStatus(GameObject):
     def lose_life(self):
         self.life -= 1
 
-class FrameUpdater(GameObject):
-    # 움직이는 객체 위치 업데이트하고, 충돌하는 객체 충돌 확인
-    bullet = Bullet()
-    enemy = Enemy()
-
-    @abstractmethod
-    def update(self):
+    def update(self, CollType):
         pass
 
-class CollisionChecker(FrameUpdater):
+class CollType(ABC):
     ps = PlayerStatus()
-    def check(self, punit, unit):
+    def activate(self):
+        pass
+
+class BulletToEnemy(CollType):
+    def activate(self, bullet, enemy):
+        bullet.delete()
+        enemy.delete()
+        self.ps.update_score()
+
+class BulletToWalls(CollType):
+    def activate(self, bullet, walls):
+        bullet.reflex()
+
+class EnemyToBottom(CollType):
+    def activate(self, bullet, walls):
+        self.ps.lose_life()
+
+class CollisionChecker:
+    ps = PlayerStatus()
+    def check(self, unit1, unit2, CollType):
         # 여기서 if문 쓰면 디미터 위반됨
-        punit.is_collision(unit) 
-        if punit.is_collision(unit):
-                bullet.delete()
-                self.ps.update_score()
+        if unit1.is_collide_at(unit2):
+            CollType.activate(unit1, unit2)
 
-        if punit.is_collision(unit):
-                enemy.delete()
-                self.ps.lose_life()
+class PositionCreater:
+    # 모든 객체 생성을 담당하는 객체
+    def create_object(self, object):
+        pass
 
-class PositionUpdater(FrameUpdater):
+class PositionUpdater:
+    # Movable 타입 객체 위치를 틱당 업데이트하는 객체
     ck = CollisionChecker()
+    pk = PositionCreater()
 
     def update(self, unit):
         for bullet in self.bullets[:]:
             bullet.update_position()
-            self.ck.check(bullet, enemy)
-            self.ck.check(bullet, GameFrame)
             
         for enemy in self.enemies[:]:
             enemy.update_position()
-            self.ck.check(enemy, GameFrame)
+        
+        self.ck.check(bullet, GameFrame, BulletToWalls)
+        self.ck.check(bullet, enemy, BulletToEnemy)
+        self.ck.check(enemy, GameFrame, EnemyToBottom)
 
 if __name__ == "__main__":
     game = ShootingGame()
